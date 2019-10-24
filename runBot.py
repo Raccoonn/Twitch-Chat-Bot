5b75af395b3fac8191f2a891dc54d68c488fbbeb
Kyle Allain <39541671+Raccoonn@users.noreply.github.com>
2019-10-24 11:56:13 -0700

    Add files via upload

---
+++ runBot.py
@@ -0,0 +1,139 @@
+#########################################################################################
+#                          bot script that runs TwitchChatBot module                    #   
+#########################################################################################
+# Console interface script that creates a Twitch Chat Bot.
+#     - See TwitchChatBot.py for more information on methods
+#
+# Begin by selecting a nickname and a channel.  Server, Port, Token can also be specified
+# but for single user the default information is ok.  Once a channel has been selected
+# input a choice from the selection below:
+#
+# Choices: ['Read Chat', 'Write Chat', 'Count Votes', 'Contest', 'Change Socket', 'Quit']
+#
+#     - Read Chat reads and displays live twitch chat from the selected channel
+#
+#     - Write Chat writes chat to a file with an option for displaying live chat
+#
+#     - Count votes stores chat data over a runtime and compares inputs to a library
+#       of keywords and tallies votes by unique user or total votes.
+#
+#     - Contest uses an input string and reads incoming chat until a matching message
+#       is found.
+#
+#     - Change Socket allows the bot to change nickname and channel
+#
+#     - Quit will close the bot
+#
+#########################################################################################
+#########################################################################################
+
+
+
+from twitchChatBot import ChatBot
+import os
+
+
+def show():
+    '''
+    Returns settings for showProgress and showChat during logging.
+    '''
+    while True:
+        display = input('\nDisplay chat during logging (Y/N): ')
+        if display.lower() == 'y':
+            showChat = True
+            showProgress = False
+            break
+        elif display.lower() == 'n':
+            showChat = False
+            showProgress = True
+            break
+    return showProgress, showChat
+
+
+
+if __name__ == '__main__':
+
+    print('\nWelcome to the Twitch chat logger, the wrong way to save cancer.\n')
+
+    # Get nickname, channel, and log display information
+    nickname = input('\nSelect a nickname: ')
+
+    while True:
+        channel = input('\nSelect a channel: ')
+        if channel[0] != '#':
+            print('\nIncorrect channel name, use form "#channelname"')
+        else:
+            break
+
+    # Initialize bot with either default or user selected credentials
+    while True:
+        server = input('\nInput server: ')
+        port = int(input('\nInput port'))
+        token = input('\nInput token')
+        bot = ChatBot(nickname, channel, server, port, token)
+        break
+
+    # Connect the bot
+    bot.connect_socket()
+
+
+    # Main loop, will run to KeyboardInterrupt or if Quit is selected from choices
+    while True:
+        # Input a task
+        while True:
+            choices = ['Read Chat', 'Write Chat', 'Count Votes', 'Contest', 'Change Socket', 'Quit']
+            pick = input('\nRead Chat, Write Chat, Count Votes, Contest, Change Socket, or Quit?  ')
+            if pick in choices:
+                break
+        
+        if pick == 'Quit':
+            break
+
+        elif pick == 'Change Socket':
+            nickname = input('\nSelect a nickname: ')
+            while True:
+                channel = input('\nSelect a channel: ')
+                if channel[0] != '#':
+                    print('\nIncorrect channel name, use form "#channelname"')
+                else:
+                    break
+            bot.change_socket(nickname, channel)
+
+
+        elif pick == 'Read Chat':
+            bot.read_chat()
+        
+        if pick not in ['Quit', 'Change Socket', 'Read Chat']:
+            # Assign showProgress, showChat for following methods
+            showProgress, showChat = show()
+
+            if pick == 'Contest':
+                winningPhrase = input('\nInput the winning phrase: ')
+                bot.contest(winningPhrase, showProgress, showChat)
+
+            else:
+                # Check for #channel_chat.log and choose to delete or not
+                if os.path.exists(channel + '_chat.log') == True:
+                    new_log = input('\nCreate fresh log file (Y/N): ')
+                    if new_log.lower() == 'y':
+                        os.remove(channel + '_chat.log')
+
+                runtime = int(input('\nInput runtime in seconds: '))
+
+                if pick == 'Write Chat':
+                    bot.write_chat(runtime, showProgress, showChat)
+
+                elif pick == 'Count Votes':
+                    voteLibrary = input('\nSpecify file for voteLibrary: ')
+                    uniqueVotes = input('\nCount unique user votes (Y/N): ')
+                    if uniqueVotes.lower() == 'y':
+                        uniqueUsers = True
+                    else:
+                        uniqueUsers = False
+
+                    talliedVotes = bot.vote_counter(runtime, voteLibrary, showProgress,
+                                                    showChat, uniqueUsers)
+                    for key in talliedVotes:
+                        print(key + ' :  %d' % talliedVotes[key])
+
+
---
+++ twitchChatBot.py
@@ -0,0 +1,298 @@
+#########################################################################################
+#                                Twitch ChatBot module                                  #
+#########################################################################################
+# Building a Twitch Chat Bot
+#
+# ChatBot class creates an instance of a Bot that connects to the specified Twitch
+# chat channel using a socket.   Once connected the Bot can read and log live chat
+# from the selected Twitch channel.  The bot can also disconnect from the current
+# channel and reconnect to another without closing the bot instance.
+#
+#   Methods:  - __init__ : Setup credentials for the ChatBot object.
+#
+#             - connect_socket : Connects a socket to irc using credentials.
+#
+#             - change_socket : Closes current socket, modifies credentials,
+#                               and reconnects to the new channel.
+#
+#             - read_chat : Prints chat to console with no logging and no runtime.
+#
+#             - write_chat : Logs chat to #channel_chat.log over the runtime.
+#
+#             - get_chat_dataframe : Returns Pandas dataframe from the #channel_chat.log.
+#
+#             - vote_counter : Using a predefined voteLibrary.txt (csv), counter will 
+#                              log chat messages then load a data frame and check
+#                              messages for instances of voteLibrary.txt strings.
+#                              Votes can be counted by unique user or all together.
+#
+#             - contest : Using a predefined winningPhrase this parses messages 
+#                         and returns the username, message if the string matches.
+#                         Can also specify a number of winners.
+#
+#
+#               **** Only reads messages that are timestamped ****
+#
+# When the reader is activated the first batch of messages are not timestamped and will
+# not be caught by the logger due to the specific syntax.  Given these missed 
+# messages are in such a small amount for longer logging sessions I am just ignoring
+# it for the time being.
+#
+#########################################################################################
+#########################################################################################
+
+
+
+import socket
+import logging
+from emoji import demojize
+import time
+import progressbar
+from datetime import datetime
+import re
+import pandas as pd
+
+
+
+class ChatBot:
+    def __init__(self, nickname, channel, server, port, token):
+        '''
+        - Sets up login credentials.  Server, port, token are all presaved.  This bot
+          is for twitch so this data does not need to change for a given user.
+
+        - Currently a single ChatBot instance should be used for a single channel,
+          you can start and stop chat once the bot has been enabled but it cannot
+          change channels.
+
+        - init creates a specific string title for files created by the bot instance,
+          the form of these files is '#channel_chat.log'.
+        '''
+        # Credentials Data
+        self.nickname = nickname
+        self.channel = channel
+        self.server = server
+        self.port = port
+        self.token = token
+
+
+
+    def connect_socket(self):
+        '''
+        Connect a socket to the specified twitch channel using the given credentials.
+        To change channel or credentials use change_socket()
+        '''
+        # Add prefix for channel specific logFile
+        self.logFile = self.channel + '_chat.log'
+
+        # Create and connect a socket
+        self.sock = socket.socket()
+        self.sock.connect((self.server, self.port))
+
+        # Connect the socket
+        self.sock.send(f"PASS {self.token}\n".encode('utf-8'))
+        self.sock.send(f"NICK {self.nickname}\n".encode('utf-8'))
+        self.sock.send(f"JOIN {self.channel}\n".encode('utf-8'))
+
+        print('\nConnection Successful')
+
+
+
+    def change_socket(self, nickname, channel, server, port, token):
+        '''
+        Closes current socket and changes to the new channel.
+        '''
+        try:
+            self.sock.close()
+
+            # Reassignment for new Credentials Data
+            self.nickname = nickname
+            self.channel = channel
+            self.server = server
+            self.port = port
+            self.token = token
+
+            self.connect_socket()
+        except:
+            print('\nNo socket to change')
+
+        
+
+    def read_chat(self):
+        '''
+        Simply reads chat and prints raw output to console with no logging.
+        '''
+        while True:
+            try:
+                resp = self.sock.recv(2048).decode('utf-8')
+                print(resp)
+            except:
+                print('\nChat reading canceled.')
+                return
+
+
+
+    def write_chat(self, runtime, showProgress=True, showChat=False):
+        '''
+        Logs chat over the runtime to the channel's logFile in the directory.
+        '''
+        # Set up logging, for loop required, wasnt working without it
+        # Could also define my own file rather than using a handler
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s - %(message)s',
+                            datefmt='%Y-%m-%d_%H:%M:%S',
+                            handlers=[logging.FileHandler(self.logFile, encoding='utf-8')])
+
+        input('\nPress Enter to begin logging\n')
+        timer_start = time.time()
+
+        # Setup progressbar
+        if showProgress == True:
+            widgets = ['Processed messages:  ', progressbar.Counter('%(value)05d'),
+                       '     (', progressbar.Timer(), ')']
+            bar = progressbar.ProgressBar(widgets=widgets)
+
+        while True:
+            # Loop during runtime and log messages as they come in
+            try:
+                resp = self.sock.recv(4096).decode('utf-8')
+
+                if showChat == True:
+                    print(resp)
+                elif showProgress == True:
+                    bar += 1  
+
+                if resp.startswith('PING'):
+                    self.sock.send('PONG\n'.encode('utf-8'))
+                elif len(resp) > 0:
+                    logging.info(demojize(resp))
+
+            except:
+                logging.shutdown()
+                print('\nChat logging canceled')
+                if showProgress == True:
+                    bar.finish()
+                return
+
+            # Return after runtime and progressbar update
+            if abs(timer_start - time.time()) >= runtime:
+                logging.shutdown()
+                if showProgress == True:
+                    bar.finish()
+                print('\n%d seconds of chat logged from' % (runtime), self.channel + '\n')
+                return
+
+            
+         
+    def get_chat_dataframe(self):
+        '''
+        Loads the current #channel_chat.log file and returns a pandas dataframe.
+        '''
+        data = []
+
+        with open(self.logFile, 'r', encoding='utf-8') as f:
+            lines = f.read().split('\n\n\n')
+            # Go through lines and extract data by splitting strings appropriately 
+            for line in lines:
+                try:
+                    time_logged = line.split(' - ')[0].strip()
+                    time_logged = datetime.strptime(time_logged, '%Y-%m-%d_%H:%M:%S')
+
+                    username_message = line.split(' - ')[1:]
+                    username_message = ''.join(username_message).strip()
+
+                    username, channel, message = re.search(
+                        ':(.*)\\!.*@.*\\.tmi\\.twitch\\.tv PRIVMSG #(.*) :(.*)',
+                        username_message).groups()
+
+                    d = {
+                        'dt': time_logged,
+                        'channel': channel,
+                        'username': username,
+                        'message': message
+                        }
+
+                    data.append(d)
+
+                except Exception:
+                    pass
+
+        return pd.DataFrame().from_records(data)
+
+    
+
+    def vote_counter(self, runtime, voteLibrary='voteLibrary.txt',
+                     showProgress=True, showChat=False, uniqueUsers=True):
+        '''
+        Counts votes for entries specified in the voteLibrary file.
+        Votes can be counted by only uniqueUserss or all votes total.
+        Returns a dictionary with vote Library keys to tallied values.
+        '''
+        # Run a logging session and get the data frame
+        self.write_chat(runtime)
+        chatLog = self.get_chat_dataframe()
+        chatLog.set_index('dt', inplace=True)
+
+        with open(voteLibrary, 'r') as f:
+            talliedVotes = {key:0 for key in f.read().split(', ')}
+
+        keys = list(talliedVotes)
+        messages = chatLog.values[:,1]
+
+        if uniqueUsers == True:
+            users = chatLog.values[:,2]
+            user_cache = []
+
+        for i in range(len(messages)):
+            for key in keys:
+                
+                # Unique votes
+                if uniqueUsers == True:
+                    if users[i] not in user_cache and key in messages[i]:
+                        talliedVotes[key] += 1
+                        user_cache.append(users[i])
+                
+                # All votes, maximum of 1 per message
+                else:
+                    if key in messages[i]:
+                        talliedVotes[key] += 1
+
+        return talliedVotes
+    
+    
+
+    def contest(self, winningPhrase, showProgress=True, showChat=False, winners=1):
+        '''
+        Reads incoming messages looking for a specific phrase, when found
+        a winner is chosen and the message is printed.
+        '''
+        matches = 0
+
+        if showProgress == True:
+            print()
+            bar = progressbar.ProgressBar(widgets=['Working: ', progressbar.AnimatedMarker()])
+
+        while True:
+            # Process raw chat inputs as they come in
+            try:
+                resp = self.sock.recv(2048).decode('utf-8')
+                username, message = re.search(':(.*)\\!.* :(.*)', resp).groups()
+
+                if winningPhrase in message:
+                    print('\n\nWe have a winner :  ' + username + '  : ' + message)
+                    matches += 1
+                    if matches == winners:
+                        return
+
+                if showChat == True:
+                    print(resp)
+                if showProgress == True:
+                    bar += 1
+
+            except:
+                print('\nContest canceled')
+                return
+
+
